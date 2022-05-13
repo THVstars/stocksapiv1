a5ae643bce7447d854631202827bc75c832a312f
THVstars <kcarolinasanchez@gmail.com>
2022-05-12 20:17:09 -0400

    Initial commit

---
+++ stocksapiv1/src/main/java/com/careerdevs/stocksapiv1/Stocksapiv1Application.java
@@ -0,0 +1,13 @@
+package com.careerdevs.stocksapiv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Stocksapiv1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Stocksapiv1Application.class, args);
+	}
+
+}
---
+++ stocksapiv1/src/test/java/com/careerdevs/stocksapiv1/Stocksapiv1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerdevs.stocksapiv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Stocksapiv1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
